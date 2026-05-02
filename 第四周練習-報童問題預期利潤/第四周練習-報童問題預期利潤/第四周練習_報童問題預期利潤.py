aeb2f19ede26bd893010264851064b7a8564dd47
eric960621-droid <eric960621@gmail.com>
2026-05-02 23:46:41 +0800

    Update 第四周練習_報童問題預期利潤.py

--- 第四周練習-報童問題預期利潤/第四周練習-報童問題預期利潤/第四周練習_報童問題預期利潤.py
+++ 第四周練習-報童問題預期利潤/第四周練習-報童問題預期利潤/第四周練習_報童問題預期利潤.py
@@ -12,7 +12,6 @@ p6 = float(input())
 p7 = float(input())
 p8 = float(input())
 
-
 max_profit = -999999.0
 best_q = 0
 
@@ -20,10 +19,8 @@ for q in range(N + 1):
    
     expected_sales = 0.0
     
-
     expected_sales += (p0 * (0 if 0 < q else q)) 
-    
-    
+     
     s0 = 0 if 0 < q else 0 
     s1 = 1 if 1 < q else q
     s2 = 2 if 2 < q else q
@@ -33,7 +30,6 @@ for q in range(N + 1):
     s6 = 6 if 6 < q else q
     s7 = 7 if 7 < q else q
     s8 = 8 if 8 < q else q
-    
     
     expected_sales = (p0*0 + p1*min(q,1) + p2*min(q,2) + p3*min(q,3) + 
                       p4*min(q,4) + p5*min(q,5) + p6*min(q,6) + 

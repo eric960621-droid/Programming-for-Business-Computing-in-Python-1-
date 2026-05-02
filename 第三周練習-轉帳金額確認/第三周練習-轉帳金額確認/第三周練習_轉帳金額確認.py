227829a91c1915176a95cc48b22887b152375d9f
eric960621-droid <eric960621@gmail.com>
2026-05-02 23:45:43 +0800

    Refactor coin change calculation code

--- 第三周練習-轉帳金額確認/第三周練習-轉帳金額確認/第三周練習_轉帳金額確認.py
+++ 第三周練習-轉帳金額確認/第三周練習-轉帳金額確認/第三周練習_轉帳金額確認.py
@@ -1,14 +1,10 @@
 a = int(input())
 change = 1000 - a
-
 coins = [500, 100, 50, 10, 5, 1]
-
 result = []
-
 for coin in coins:
     count = change // coin   # 可以用幾張
     if count > 0:
         result.append(f"{coin}, {count}") #f-string:將變數塞進字串裡面(f"{coin}:{count}")
         change = change % coin   # 剩下的錢
-
 print("; ".join(result))

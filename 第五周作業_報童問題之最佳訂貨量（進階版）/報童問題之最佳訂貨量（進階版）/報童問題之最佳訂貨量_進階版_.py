79d82f5bc714c9bbcc99ce4b79029bfd95e7e082
eric960621-droid <eric960621@gmail.com>
2026-05-02 23:48:02 +0800

    Update 報童問題之最佳訂貨量_進階版_.py

--- 第五周作業_報童問題之最佳訂貨量（進階版）/報童問題之最佳訂貨量（進階版）/報童問題之最佳訂貨量_進階版_.py
+++ 第五周作業_報童問題之最佳訂貨量（進階版）/報童問題之最佳訂貨量（進階版）/報童問題之最佳訂貨量_進階版_.py
@@ -2,13 +2,11 @@ import sys
 import math
 
 def solve():
-    try:
-        
+    try:    
         c = int(input().strip())  
         r = int(input().strip()) 
         N = int(input().strip()) 
-        s = int(input().strip()) 
-        
+        s = int(input().strip())     
        
         p = []
         for _ in range(N + 1):
@@ -19,26 +17,20 @@ def solve():
     best_q = 0
     max_expected_profit = -float('inf')
     raw_max_profit = -float('inf')
-    
 
     for q in range(N + 1):
-        expected_profit = 0.0
-        
+        expected_profit = 0.0       
        
         for D in range(N + 1):
             sales = min(q, D)           
-            leftover = max(q - D, 0)    
-           
+            leftover = max(q - D, 0)             
           
-            profit = (r * sales) - (c * q) + (s * leftover)
-            
+            profit = (r * sales) - (c * q) + (s * leftover)            
            
-            expected_profit += p[D] * profit
-            
+            expected_profit += p[D] * profit            
        
         rounded_expected_profit = round(expected_profit, 4)
-        
-       
+           
         if rounded_expected_profit > max_expected_profit:
             max_expected_profit = rounded_expected_profit
             best_q = q
